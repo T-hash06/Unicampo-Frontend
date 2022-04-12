962be5d112f8b282162a11ef8e4a7f315d9d07d1
T-Hash06 <tomasfelipe.p@gmail.com>
2022-04-11 22:53:25 -0500

    [CREATE] About Page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,210 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="main-about-container">
+		<Navbar />
+		<div class="main-division">
+			<div class="mision-container">
+				<h1 class="title">- Our Mission -</h1>
+				<p class="text">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti animi aliquam, blanditiis id quibusdam laboriosam
+					dolores quae officia soluta magni odio numquam voluptatum repellendus recusandae perspiciatis. At assumenda culpa
+					aperiam quibusdam nesciunt iusto minus id. Explicabo inventore repellat ad, reiciendis eum qui. Debitis eos nulla unde
+					ipsum cum, autem laudantium excepturi suscipit vero facilis cupiditate sequi praesentium eum sint nostrum error
+					necessitatibus voluptatibus rem ipsa. Iste animi id nulla sequi quae quam quasi ducimus vel harum dolorum repellat
+					temporibus, et dolor accusamus accusantium. Eaque magnam molestiae illum ea optio totam, voluptas inventore itaque
+					repellat reprehenderit cupiditate est dolore sapiente minus eveniet fugit alias aperiam adipisci. Necessitatibus dolores
+					tempora libero exercitationem.
+				</p>
+			</div>
+			<div class="vision-container">
+				<h1 class="title">- Our Vision -</h1>
+				<p class="text">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti animi aliquam, blanditiis id quibusdam laboriosam
+					dolores quae officia soluta magni odio numquam voluptatum repellendus recusandae perspiciatis. At assumenda culpa
+					aperiam quibusdam nesciunt iusto minus id. Explicabo inventore repellat ad, reiciendis eum qui. Debitis eos nulla unde
+					ipsum cum, autem laudantium excepturi suscipit vero facilis cupiditate sequi praesentium eum sint nostrum error
+					necessitatibus voluptatibus rem ipsa. Iste animi id nulla sequi quae quam quasi ducimus vel harum dolorum repellat
+					temporibus, et dolor accusamus accusantium. Eaque magnam molestiae illum ea optio totam, voluptas inventore itaque
+					repellat reprehenderit cupiditate est dolore sapiente minus eveniet fugit alias aperiam adipisci. Necessitatibus dolores
+					tempora libero exercitationem.
+				</p>
+			</div>
+		</div>
+
+		<div class="group-division">
+			<h1 class="title">Leadership</h1>
+			<div class="member-div">
+				<div class="image-container">
+					<img src="@/assets/logo.svg" alt="" />
+				</div>
+				<div class="title-div">
+					<h1>Vue Company</h1>
+					<h3>Developer</h3>
+				</div>
+				<p class="description">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. A natus totam itaque error molestiae quis magnam ab nemo nulla
+					rem ipsum illum consequatur omnis, sunt, quasi id quisquam reprehenderit optio.
+				</p>
+				<div class="favorite-apps">
+					<h3>Favorite Apps</h3>
+					<p>Lorem ipsum dolor sit amet.</p>
+				</div>
+			</div>
+			<div class="member-div">
+				<div class="image-container">
+					<img src="@/assets/logo.svg" alt="" />
+				</div>
+				<div class="title-div">
+					<h1>Vue Company</h1>
+					<h3>Developer</h3>
+				</div>
+				<p class="description">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. A natus totam itaque error molestiae quis magnam ab nemo nulla
+					rem ipsum illum consequatur omnis, sunt, quasi id quisquam reprehenderit optio.
+				</p>
+				<div class="favorite-apps">
+					<h3>Favorite Apps</h3>
+					<p>Lorem ipsum dolor sit amet.</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+</script>
+
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.main-about-container {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+}
+
+.main-division,
+.group-division {
+	box-shadow: 0px 0px 20px #000;
+	width: 100%;
+	max-width: 1024px;
+
+	background-color: #101010;
+	padding: 80px 0px;
+	margin-top: 40px;
+
+	border-radius: 10px;
+}
+
+.mision-container,
+.vision-container {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+
+	margin: 0px 0px 100px 0px;
+}
+
+.mision-container .title,
+.vision-container .title,
+.group-division .title {
+	font-size: 48px;
+	font-weight: 100;
+	color: #ebebeb;
+	text-align: center;
+	letter-spacing: 1px;
+
+	width: 100%;
+	margin-bottom: 20px;
+}
+
+.mision-container .text,
+.vision-container .text {
+	font-size: 20px;
+	font-weight: 300;
+	color: #cccccc;
+
+	width: 80%;
+	line-height: 36px;
+
+	text-align: justify;
+}
+
+.group-division {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	gap: 60px;
+
+	margin-bottom: 40px;
+}
+
+.member-div {
+	display: grid;
+	grid-template-rows: 170px 90px auto 86px;
+	width: 70%;
+	max-width: 430px;
+	height: 490px;
+}
+
+.member-div .image-container {
+	justify-self: center;
+	box-shadow: 0px 0px 20px #000;
+	border-radius: 50%;
+
+	width: 150px;
+	height: 150px;
+	justify-self: center;
+	align-self: center;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	overflow: hidden;
+}
+
+.member-div .image-container img {
+	width: 100%;
+	object-fit: cover;
+}
+
+.member-div .title-div {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	align-content: center;
+}
+
+.member-div .title-div h1 {
+	width: 100%;
+	text-align: center;
+	color: #74e39a;
+	font-weight: 600;
+}
+.member-div .title-div h3 {
+	color: #e2e2e2;
+	font-size: 19px;
+}
+
+.member-div .description {
+	font-size: 17px;
+	width: 80%;
+	justify-self: center;
+	align-self: center;
+	text-align: center;
+	color: #c2c2c2;
+}
+
+.member-div .favorite-apps {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	align-content: center;
+	gap: 4px;
+}
+
+.member-div .favorite-apps h3 {
+	color: #74e39a;
+	width: 100%;
+	text-align: center;
 }
 </style>
