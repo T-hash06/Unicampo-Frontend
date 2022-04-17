c86b4a343914a5186629967e009557ec5c9b18c1
T-Hash06 <tomasfelipe.p@gmail.com>
2022-04-17 11:06:23 -0500

    [UPDATE] Public resources

--- src/components/Sections.vue
+++ src/components/Sections.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="main-sections-container">
-		<ProductSection title="Section" image="src/assets/shop/fruits.png" :scale="1.18" />
-		<ProductSection title="Section" image="src/assets/shop/vegetables.png" :scale="1.22" reverse />
-		<ProductSection title="Section" image="src/assets/shop/meat.png" :scale="1.21" />
+		<ProductSection title="Section" image="./shop/fruits.png" :scale="1.18" />
+		<ProductSection title="Section" image="./shop/vegetables.png" :scale="1.22" reverse />
+		<ProductSection title="Section" image="./shop/meat.png" :scale="1.21" />
 	</div>
 </template>
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	publicPath: "/",
+	publicPath: "/~1000092734/",
 };
